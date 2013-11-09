ad7a16b676ec45f40b8e6de9c861eb298b3021ba
csoymm <csoymm@gmail.com>
2013-11-09 15:47:27 -0500

    added gunicorn dependency to requirements.txt

--- servbot/views.py
+++ servbot/views.py
@@ -41,77 +41,75 @@ def createTable(restaurant, number):
 	return json.dumps(db.tables.find_one({"owner" : restaurant, "number" : number})["number"])
 
 
-@app.route('/order/', methods=['get', 'post'])
-def addorders():
+@app.route('/order/<restaurant>/<table>/<order>', methods=['get', 'post'])
+def addorders(restaurant, table, order):
  
 	client = MongoClient('mongodb://admin:admin@ds053678.mongolab.com:53678/servbot')
 	db = client.servbot
-	price = db.tables.find({"number" : 2, "owner" : "TGI Fridays"})[0]["price"]
-	print price
-	price += db.menus.find({"owner" : "TGI Fridays", "number" : 3})[0]["price"]
-	print price
+	price = db.tables.find_one({"number" : table, "owner" : restaurant})[0]["price"]
+	price += db.menus.find({"owner" : restaurant, "number" : order})[0]["price"]
 	db.tables.update(
-		{"number" : 2, "owner" : "TGI Fridays"},
-		{ "$push" : {"orders" : "woohoo"},
+		{"number" : table, "owner" : restaurant},
+		{ "$push" : {"orders" : order }
 		}
 	)
 
 	db.tables.update(
-		{"number" : 2, "owner" : "TGI Fridays"},
+		{"number" : table, "owner" : restaurant},
 		{ "$set" : {"price" : price},
 		}
 	)
 	return peekOrder()	
 
 
-@app.route('/peekOrder/', methods=['get', 'post'])
-def peekOrder():
+@app.route('/peekOrder/<restaurant>/<order>', methods=['get', 'post'])
+def peekOrder(restaurant, order):
  
 	client = MongoClient('mongodb://admin:admin@ds053678.mongolab.com:53678/servbot')
 	db = client.servbot
- 	return json.dumps(db.tables.find_one({'owner' : "TGI Fridays", 'number': 2})["orders"])
+ 	return json.dumps(db.tables.find_one({'owner' : restaurant, 'number': order})["orders"])
 
 
 
-@app.route('/insertToMenu/', methods=['get', 'post'])
-def addMenus():
+@app.route('/insertToMenu/<restaurant>/<order>/<description>/<price>', methods=['get', 'post'])
+def addMenus(restaurant, order, description,price):
  
 	client = MongoClient('mongodb://admin:admin@ds053678.mongolab.com:53678/servbot')
 	db = client.servbot
 
-	db.menus.insert({"owner" : 'TGI Fridays',
-		"number" : 3,
-		"description" : "ribs",
-		"price" : 13.99
+	db.menus.insert({"owner" : restaurant,
+		"number" : order,
+		"description" : description,
+		"price" : price
 })
-	return json.dumps(db.menus.find_one({"owner" : 'TGI Fridays', "number" : 3})["description"])
+	return json.dumps(db.menus.find_one({"owner" : restaurant, "number" : order})["description"])
 
 
-@app.route('/menus/', methods=['get', 'post'])
-def getMenus():
+@app.route('/menus/<restaurant>', methods=['get', 'post'])
+def getMenus(restaurant):
  	client = MongoClient('mongodb://admin:admin@ds053678.mongolab.com:53678/servbot')
 	db = client.servbot
 
 	menu = []
-	cursor2 = db.menus.find({"owner" : 'TGI Fridays'}).sort("number" , 1)
+	cursor2 = db.menus.find({"owner" : restaurant}).sort("number", 1) 
 	for c in cursor2:
-		menu.append(c["description"])
+		menu.append(json.dumps({"name" : c["description"], "price" : c["price"]}))
  	return json.dumps(menu)
 
-@app.route('/getPrice/', methods=['get', 'post'])
-def getPrice():
+@app.route('/getPrice/<restaurant>', methods=['get', 'post'])
+def getPrice(restaurant):
  	client = MongoClient('mongodb://admin:admin@ds053678.mongolab.com:53678/servbot')
 	db = client.servbot
 
-	cursor4 = db.menus.find_one({"owner" : 'TGI Fridays'})
+	cursor4 = db.menus.find_one({"owner" : restaurant})
  	return json.dumps(cursor4["price"])
 
 
 
-@app.route('/checkoutPrice/', methods=['get', 'post'])
-def checkoutPrice():
+@app.route('/checkoutPrice/<restaurant>/<table>', methods=['get', 'post'])
+def checkoutPrice(restaurant, table):
  	client = MongoClient('mongodb://admin:admin@ds053678.mongolab.com:53678/servbot')
 	db = client.servbot
- 	return json.dumps(db.tables.find_one({"number" : 2, "owner" : "TGI Fridays"})["price"])
+ 	return json.dumps(db.tables.find_one({"number" : "200", "owner" : "Applebees"})["price"])
 
 

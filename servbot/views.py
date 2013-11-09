4bcbaa971a9d4e284c3692797177d8275c23a278
csoymm <csoymm@gmail.com>
2013-11-09 07:14:39 -0500

    integrate PyMongo. Defined function that returns jSON list of restaurant names

--- servbot/views.py
+++ servbot/views.py
@@ -2,8 +2,52 @@
 from servbot import app
 from flask import render_template
 import helpers
+from flask.ext.pymongo import PyMongo
+import pymongo
+from pymongo import MongoClient
+import json
+
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	return render_template('landing.html');
+
+
+
+@app.route('/restaurants/', methods=['GET', 'POST'])
+def restaurantList():
+	
+#app.config['MONGO_URI'] = 'mongodb://admin:admin@ds053678.mongolab.com:53678/servbot'
+#app.config['MONGO_DBNAME'] = 'restaurants'
+#app.config['MONGO_USERNAME'] = 'admin'
+#app.config['MONGO_PASSWORD'] = 'admin'
+
+#mongo = PyMongo(app, config_prefix = 'MONGO');
+	client = MongoClient('mongodb://admin:admin@ds053678.mongolab.com:53678/servbot')
+	db = client.servbot
+
+#db.restaurants.insert({"name" : '',
+#"tables" : [{
+#	"number" : '',
+#	"orders" : []
+#		}
+#	]
+#})
+#	db.restaurants.update(
+#		{ "name" : ""
+#		},
+#		{ "$push" : {"tables.0.orders" : "woohoo"}
+#		}
+#	)
+	index = 0
+	cursor = db.restaurants.find().sort("name", 1)
+	nameList = []
+	for v in cursor:
+		print v['name']
+		nameList.append(v['name'])
+
+	for greg in nameList:
+		print ("%s" % greg)
+
+	return json.dumps(nameList)
